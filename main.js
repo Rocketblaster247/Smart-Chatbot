cb626455b55935088469bafe98e8b7e6273c4125
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-07-18 16:12:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-
+var json;
+var tag_dataset = [];
+var net = new brain.recurrent.LSTM();
+var load = function (d) {
+  tag_dataset = d.toString().split(";");
+    net.train(tag_dataset, {
+      iterations: 300,
+      errorThresh: 0.02,
+      log: true,
+      logPeriod: 1,
+      learningRate: 0.2,
+      momentum: 1,
+      callback: null,
+      callbackPeriod: 10,
+      timeout: 1000*60,
+    });
+};
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "tag-dataset");
+xhr.addEventListener("load", function (x) {
+    console.log("Loaded file...");
+    load(this.responseText);
+});
+xhr.send();
